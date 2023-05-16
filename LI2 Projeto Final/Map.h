f573d7970ed5dc2e3698665477cda0846246b369
Fernando Pires <120588524+ferjpires@users.noreply.github.com>
2023-05-16 15:30:13 +0100

    Add files via upload

---
+++ LI2 Projeto Final/Map.h
@@ -0,0 +1,73 @@
+#ifndef MAP_H
+#define MAP_H
+
+
+
+
+
+/*
+função que apoia a criação do mapa juntando paredes
+*/
+void do_concat_walls(int linhas, int colunas, Map mapa[][colunas]);
+/*
+esta funçao cria o mapa inicial
+falta completar fazendo com que crie estruturas em condiçoes, juntas e nao apenas randomizadas - feito
+falta acrescentar lagos, muniçoes, etc
+*/
+void do_create_map(int linhas, int colunas, Map mapa[][colunas], Flag *flag, Game *game);
+/*
+esta funçao é muito simples e apenas imprime o mapa para o ecra do utilizador
+*/
+void do_print_map(int linhas, int colunas, Map mapa[][colunas]);
+
+void print_footer_single_player(Player *player1);
+void print_footer_multi_player(int linhas, int colunas, Player *player1, Player *player2);
+void print_footer_challenge(Player *player1);
+
+/*
+funçao que destroi uma parede na direçao onde se moveu pela ultima vez
+*/
+void do_destroy_wall(char last_direction_moved, int y, int x, int linhas, int colunas, Map mapa[][colunas]);
+
+void create_light_lighthouse(int linhas, int colunas, Map mapa[][colunas],Mob *mobs);
+
+/*
+funçao da luz / escuridão
+    ------------------360 LINHAS?------------------
+*/
+void createlight(int posy, int posx, int colunas, int linhas, int z, Player *player1);
+/*
+funçao que acrescenta um lago ao mapa
+*/
+void do_add_lake(int linhas, int colunas, Map mapa[][colunas]);
+/*
+funçao que acrescenta casa de curar vida
+*/
+void do_add_life(int linhas, int colunas, Map mapa[][colunas]);
+/*
+funçao que acrescenta casa de aumentar muniçao
+*/
+void do_add_ammo(int linhas, int colunas, Map mapa[][colunas]);
+/*
+funçao que adiciona o farol
+*/
+void do_add_lighthouse(int linhas, int colunas, Map mapa[][colunas]);
+
+/*
+função que conta quantas paredes existem num quadrado 3x3 do mapa
+*/
+int count_walls_3x3(int colunas, Map mapa[][colunas], int linha_atual, int coluna_atual);
+/*
+função que conta quantas paredes existem num quadrado 5x5 do mapa
+*/
+int count_walls_5x5(int colunas, Map mapa[][colunas], int linha_atual, int coluna_atual);
+/*
+função que conta quantas paredes existem num quadrado 5x5 do mapa
+*/
+int count_walls_4x8(int colunas, Map mapa[][colunas], int linha_atual, int coluna_atual);
+/*
+função que insere a flag no mapa
+*/
+void do_insert_flag(int linhas, int colunas, Map mapa[][colunas], Flag *flag, Game *game);
+
+#endif
---
+++ LI2 Projeto Final/Player.h
@@ -0,0 +1,31 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+
+
+
+void player1_position(int linhas, int colunas, Player *player1);
+void player2_position(int linhas, int colunas, Player *player2);
+void do_add_player(int game_type, Player *player1, Player *player2);
+int player_relation_to_mob(int movement, Mob *mobs, Player *player1);
+int valid_player_movement(int movement, int colunas, Map mapa[][colunas], Player *player1, Mob *mobs);
+
+/*
+Ve se o Player esstá a um de distanica do mob
+*/
+void close_to_player(Player *player1, Mob *mobs);
+// A direção do ataque está relacionada com o ultimo movimento do jogador
+void do_player_punch(Game *game, int linhas, int colunas, Map mapa[][colunas], Player *player1, Mob *mobs);
+
+void do_update_map_single_player(int colunas, Map mapa[][colunas], int linhas, Game *game, Player *player1, Mob *mobs);
+void do_update_map_multi_player(int colunas, Map mapa[][colunas], int linhas, Game *game, Player *player1, Player *player2);
+void do_add_score(int game_type, Flag *flag, Game *game, Player *player1, Player *player2);
+
+/*
+funçao relativas às estruturas que tiram e acrescentam vida/amunição
+*/
+void do_structure_aplications_single_player(int linhas, int colunas, Map mapa[][colunas], Player *player1, Mob *mobs);
+void do_structure_aplications_multi_player(int colunas, Map mapa[][colunas], Player *player1, Player *player2);
+
+#endif
